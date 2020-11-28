527346d8585a7766de483eef7506a1ca9eec23e5
Fuchssystems <fuchs@fuchs.systems>
2020-11-28 14:38:00 +0100

    kernel.php throttle disabled

--- routes/api.php
+++ routes/api.php
@@ -30,6 +30,7 @@ Route::group(['middleware' => ['auth:api']], function () {
   Route::patch('user',[UserController::class, 'update']);
   Route::put('deleteWebuser',[UserController::class, 'delete']);
   Route::put('addFileProfile', [FileController::class, 'addPicture']);
+  Route::put('getAllProfileFiles', [FileController::class, 'getAllProfileFiles']);
   Route::put('loadFile', [FileController::class, 'loadFile']);
   Route::put('deleteFile', [FileController::class, 'deleteFile']);
   Route::put('deleteFiles', [FileController::class, 'deleteFiles']);
@@ -39,8 +40,4 @@ Route::group(['middleware' => ['auth:api']], function () {
   Route::put('chatProfileSearch', [ProfileController::class, 'chatProfileSearch']);
   
   Route::put('test',[UserController::class, 'test']);
-});
-// fetching of profile images allowed 600 times per minute (100 ms)
-Route::group(['middleware' => ['auth:api', 'throttle: 600']], function () {
-  Route::put('getAllProfileFiles', [FileController::class, 'getAllProfileFiles']);
 });
